83345657cac0772d5b4e86329ccc6f16213d452d
MaGiC <maxvgc@gmail.com>
2012-04-26 22:46:34 -0500

    licensed under apache 2.0

--- part3/script.js
+++ part3/script.js
@@ -3,6 +3,20 @@ These are the functions specific to part three. First are the functions
 that either allow or disallow continuation to the result page,
 depending on user input. Then are the handlers for user input. Last is
 part that actually does the computation and fills in the results.
+
+Copyright 2012 Maxwell Galloway-Carson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 /*                                                                            
