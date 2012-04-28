cf57ece12b55acf0711af63e4c7ecaa735e51789
MaGiC <maxvgc@gmail.com>
2012-04-27 23:52:57 -0500

    made agpl

--- part4/script.js
+++ part4/script.js
@@ -4,19 +4,23 @@ that either allow or disallow continuation to the result page,
 depending on user input. Then are the handlers for user input. Last is
 part that actually does the computation and fills in the results. 
 
-Copyright 2012 Maxwell Galloway-Carson
+This file is part of Panels and Turbines
+Copyright (C) 2012  Peter Schwartzman and Max Galloway-Carson
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+Panels and Turbines is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-http://www.apache.org/licenses/LICENSE-2.0
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+email me at: maxvgc@gmail.com
 */
 
 /*                                                                            
